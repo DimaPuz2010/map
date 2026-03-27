6097436ff941ff50c492b0120d4b927f08538dc0
DimaPuz2010 <91826731+DimaPuz2010@users.noreply.github.com>
2026-03-27 17:47:55 +0600

    clearning code change api key add singUp

--- app/src/main/java/com/example/map/data/network/Api.kt
+++ app/src/main/java/com/example/map/data/network/Api.kt
@@ -2,25 +2,17 @@ package com.example.map.data.network
 
 import com.example.map.data.network.model.Auth
 import com.example.map.data.network.model.AuthResspons
-import com.example.map.data.network.model.RecommendationRequestDto
-import com.example.map.data.network.model.RecommendationResponseDto
 import com.example.map.domain.model.Recommendation
 import com.example.map.domain.model.UserProfile
 import retrofit2.http.Body
 import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.Header
+import retrofit2.http.PATCH
 import retrofit2.http.POST
 import retrofit2.http.Query
 
 interface Api {
-    @POST("rest/v1//recommendations/nearby")
-    suspend fun getRecommendations(
-        @Body request: RecommendationRequestDto,
-        @Header("Authorization") auth: String = "",
-        @Header("apikey") apiKey: String = "sb_publishable_8F_XgYfzMI0Saj6xY7kcAQ_dK4P3vuz"
-    ): RecommendationResponseDto
-
     @POST("auth/v1/signup")
     suspend fun singUp(
         @Header("apikey") apiKey: String = "sb_publishable_8F_XgYfzMI0Saj6xY7kcAQ_dK4P3vuz",
@@ -39,6 +31,14 @@ interface Api {
         @Header("Authorization") auth: String = "",
         @Query("id")  id: String = "eq.1"
     ): List<UserProfile>
+
+    @PATCH("rest/v1/profils")
+    suspend fun changeProfile(
+        @Header("apikey") apiKey: String = "sb_publishable_8F_XgYfzMI0Saj6xY7kcAQ_dK4P3vuz",
+        @Header("Authorization") auth: String = "",
+        @Query("id") id: String,
+        @Body userProfile: UserProfile
+    )
 
     @POST("rest/v1/recomindation")
     suspend fun createRecommendation(
